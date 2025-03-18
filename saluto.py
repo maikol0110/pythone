84bc9977505e51c142ec56a32ee506b93f83f9a1
Maikol Freddari <maikolfreddari94@gmail.com>
2025-03-18 13:02:04 +0100

    somma

--- saluto.py
+++ saluto.py
@@ -1,4 +1,12 @@
 #print(Ciao, mi chiamo Maikol)
 
-nome="Maikol Freddari"
-print("Mi chiamo", nome)
+#nome="Maikol Freddari"
+#print("Mi chiamo", nome)
+
+valore_1 = input("Passami il primo valore: ")
+valore_2 = input ("Passami il secondo valore: ")
+v1 =int(valore_1)
+v2= int(valore_2)
+
+somma=valore_1+valore_2
+print("La somma di valore1 e valore2 è:", somma)
